2bbb0561b4f212a9ad762c2268fb12bc99131e1c
btor <the.btor@gmail.com>
2019-11-14 23:58:25 +0100

    Wait until dom is edited

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-document.querySelectorAll('section.content').forEach((el) => el.style.filter='none');
-document.querySelector('[id^=offer-]').style.display='none';
+const observerConfig = {
+    childList: true,
+    subtree: true,
+    attributes: false,
+    characterData: false
+};
+
+const unblurify = () => {
+    console.log('Unblurifing...');
+    document.querySelectorAll('section.content').forEach((el) => el.style.filter = 'none');
+    const offer = document.querySelector('[id^=offer-]');
+    if (offer) {
+        offer.style.display = 'none';
+    }
+};
+
+const observer = new MutationObserver(unblurify);
+observer.observe(document, observerConfig);
+
+
