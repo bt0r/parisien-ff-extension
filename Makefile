9b87bcdf2988d3cacd7ab67b6009b1c5573b1a56
btor <the.btor@gmail.com>
2019-11-14 23:13:57 +0100

    initial commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+deploy:
+	web-ext sign --api-key=user:13397420:121 --api-secret=c9d685a294bc0a2be064a40871d567a21c6c1603a6df15d55c28939b145a8b4d
---
+++ index.js
@@ -0,0 +1,2 @@
+document.querySelectorAll('section.content').forEach((el) => el.style.filter='none');
+document.querySelector('[id^=offer-]').style.display='none';
